b26bcbd915d8b60e8b510d5bacd1bf7eeca8f5e2
Michael Eyler <eyler94@gmail.com>
2022-02-01 14:53:19 -0700

    Got the analog read working on this pin.

--- CrystalCavernChaos/ArduinoUnitTests/AnalogRead/AnalogRead.ino
+++ CrystalCavernChaos/ArduinoUnitTests/AnalogRead/AnalogRead.ino
@@ -1,9 +1,13 @@
+int const photoDiode{35};
+
 void setup() {
   // put your setup code here, to run once:
-  
+  pinMode(photoDiode, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println(analogRead(photoDiode));
+  delay(500);  
 }
