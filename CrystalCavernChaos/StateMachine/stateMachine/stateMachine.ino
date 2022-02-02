46e374ec98a9941cd86bfe22b4afdadcaeb348f7
ELC Shop Techies <ecenshop@gmail.com>
2022-02-02 10:15:26 -0700

    Started state machine.

---
+++ CrystalCavernChaos/StateMachine/stateMachine/stateMachine.ino
@@ -0,0 +1,15 @@
+const int numServos{7};
+const int servoList[numServos]{12, 14, 25, 26, 27, 32, 33};
+const int irLed{13};
+const int photoDiodeLed{5};
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Connecting Servos");
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
