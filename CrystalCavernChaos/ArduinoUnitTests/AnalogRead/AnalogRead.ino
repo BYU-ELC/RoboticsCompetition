0d2af3ed130f1738cf04e5301abddc2dfc0086fd
Michael Eyler <eyler94@gmail.com>
2022-02-02 08:46:45 -0700

    Finished configuring the analog read and the ir LED.

--- CrystalCavernChaos/ArduinoUnitTests/AnalogRead/AnalogRead.ino
+++ CrystalCavernChaos/ArduinoUnitTests/AnalogRead/AnalogRead.ino
@@ -1,13 +1,34 @@
 int const photoDiode{35};
+int const irLED{13};
+
+int timePrevious{};
+int timeCurrent{};
+int timeDiff{1000};
+
+bool ledState{true};
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(photoDiode, INPUT);
+  pinMode(irLED, OUTPUT);
   Serial.begin(9600);
+  timePrevious = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:  
+  timeCurrent = millis();
+  if (timeCurrent-timePrevious >= timeDiff) {
+    if (ledState) {
+      digitalWrite(irLED, LOW);
+      ledState = false;
+    }
+    else {
+      digitalWrite(irLED, HIGH);
+      ledState = true;      
+    }
+    timePrevious = millis();
+  }
   Serial.println(analogRead(photoDiode));
-  delay(500);  
+  delay(200);  
 }
