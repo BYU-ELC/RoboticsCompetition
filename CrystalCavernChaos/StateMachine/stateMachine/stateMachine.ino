d10f94932b70e167283e556c9695338157a970f9
ELC Shop Techies <ecenshop@gmail.com>
2022-02-02 10:32:33 -0700

    added button pins to state machine code and pinout list

--- CrystalCavernChaos/StateMachine/stateMachine/stateMachine.ino
+++ CrystalCavernChaos/StateMachine/stateMachine/stateMachine.ino
@@ -1,7 +1,10 @@
 const int numServos{7};
 const int servoList[numServos]{12, 14, 25, 26, 27, 32, 33};
+const int buttonList[numServos]{2, 4, 5, 15, 16, 17, 18};
+
 const int irLed{13};
 const int photoDiodeLed{5};
+const int startStop{0};
 
 void setup() {
   Serial.begin(9600);
