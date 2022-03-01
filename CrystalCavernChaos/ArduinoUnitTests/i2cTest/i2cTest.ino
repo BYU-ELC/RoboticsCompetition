c81a1d3b1c7498aeea7d0c1a8168721fd6c9c8ad
Derekbenj <derekbenjward@gmail.com>
2022-03-01 13:02:42 -0700

    Working ArduinoUnitTest scanning and testing!

---
+++ CrystalCavernChaos/ArduinoUnitTests/i2cTest/i2cTest.ino
@@ -0,0 +1,27 @@
+// if you have jibberish getting printed: https://rntlab.com/question/lcd_i2c-esp32-random-jibberish/
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+int lcdColumns = 16;
+int lcdRows = 2;
+
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+
+void setup(){
+  // initialize LCD
+  lcd.init();
+  // turn on LCD backlight                      
+  lcd.backlight();
+}
+
+void loop(){
+  lcd.clear();
+  delay(1000);
+  // set cursor to first column, first row
+  lcd.setCursor(0, 0);
+  // print message
+  lcd.print("Hello, Derek!");
+  delay(1000);
+  // clears the display to print new message
+}
